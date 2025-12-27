8ee81f04c291d3e6f45a0397b082a33cc7fef676
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-28 00:21:03 +0100

    Add event listener to close modal on button click

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const modalEl = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
 
 
 setTimeout(() => {
     modalEl.style.display = 'inline'
 }, 1500)
+
+modalCloseBtn.addEventListener('click',() => {
+    modalEl.style.display = 'none'
+})
