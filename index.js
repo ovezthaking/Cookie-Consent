5f5cee9b41e568dc4c6d20357c534f2f463c75ee
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-28 01:51:51 +0100

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modalEl = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 
 setTimeout(() => {
@@ -13,5 +14,28 @@ modalCloseBtn.addEventListener('click',() => {
 
 consentForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('form submitted')
+    modalText.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="upload-text">
+                Uploading your data to the dark web...
+            </p>
+        </div>
+    `
+
+    setTimeout(() => {
+        document.getElementById('upload-text').innerText = 'Making the sale...'
+    }, 1500)
+
+    setTimeout(() => {
+        document.getElementById('modal-inner').innerHTML = `
+            <h2>Thanks you sucker! </h2>
+            <p>We just sold the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+                <img src="images/pirate.gif">
+            </div>
+        `
+    }, 3000)
+    
 })
+
