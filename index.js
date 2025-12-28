2bbf6d1fc281a37bb1a9b779bf34062ad2fc42e8
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-28 01:27:25 +0100

    Add consent form to modal for user data collection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const modalEl = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
 
 
 setTimeout(() => {
@@ -8,4 +9,9 @@ setTimeout(() => {
 
 modalCloseBtn.addEventListener('click',() => {
     modalEl.style.display = 'none'
+})
+
+consentForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('form submitted')
 })
