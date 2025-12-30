26fc70b96e19c540e95453922da7ba2eba4241d9
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-30 02:41:53 +0100

    Add formdata

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ modalCloseBtn.addEventListener('click',() => {
 
 consentForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    const consentFormData = new FormData(consentForm)
+    console.log(consentFormData)
+
     modalText.innerHTML = `
         <div class="modal-inner-loading">
             <img src="images/loading.svg" class="loading">
