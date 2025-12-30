8ae4f824d8663f73c39a0af5bcec411a1b4f34ba
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-30 03:14:50 +0100

    Add "Decline" button to modal choice buttons i change their direction

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
-const modalEl = document.getElementById('modal')
-const modalCloseBtn = document.getElementById('modal-close-btn')
-const consentForm = document.getElementById('consent-form')
-const modalText = document.getElementById('modal-text')
+    const modalEl = document.getElementById('modal')
+    const modalCloseBtn = document.getElementById('modal-close-btn')
+    const consentForm = document.getElementById('consent-form')
+    const modalText = document.getElementById('modal-text')
+    const declineBtn = document.getElementById('decline-btn')
+    const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
-
-setTimeout(() => {
-    modalEl.style.display = 'inline'
-}, 1500)
-
-modalCloseBtn.addEventListener('click',() => {
-    modalEl.style.display = 'none'
-})
-
-consentForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const consentFormData = new FormData(consentForm)
-    
-    const fullName = consentFormData.get('fullName')
-
-
-    modalText.innerHTML = `
-        <div class="modal-inner-loading">
-            <img src="images/loading.svg" class="loading">
-            <p id="upload-text">
-                Uploading your data to the dark web...
-            </p>
-        </div>
-    `
 
     setTimeout(() => {
-        document.getElementById('upload-text').innerText = 'Making the sale...'
+        modalEl.style.display = 'inline'
     }, 1500)
 
-    setTimeout(() => {
-        document.getElementById('modal-inner').innerHTML = `
-        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
-        <div class="idiot-gif">
-            <img src="images/pirate.gif">
-        </div>
-        `
-        modalCloseBtn.disabled = false
-    }, 3000)
-    
-})
+    modalCloseBtn.addEventListener('click',() => {
+        modalEl.style.display = 'none'
+    })
 
+    declineBtn.addEventListener('mouseover', () => {
+        modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
+    })
+
+    consentForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const consentFormData = new FormData(consentForm)
+        
+        const fullName = consentFormData.get('fullName')
+
+
+        modalText.innerHTML = `
+            <div class="modal-inner-loading">
+                <img src="images/loading.svg" class="loading">
+                <p id="upload-text">
+                    Uploading your data to the dark web...
+                </p>
+            </div>
+        `
+
+        setTimeout(() => {
+            document.getElementById('upload-text').innerText = 'Making the sale...'
+        }, 1500)
+
+        setTimeout(() => {
+            document.getElementById('modal-inner').innerHTML = `
+            <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+            <p>We just sold the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+                <img src="images/pirate.gif">
+            </div>
+            `
+            modalCloseBtn.disabled = false
+        }, 3000)
+        
+    })
